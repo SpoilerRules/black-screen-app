9cc4e77d060780ba4f503811e027c0dab958b3a3
SpoilerRules <spoilerrulesowo@gmail.com>
2024-04-27 22:58:48 +0200

    fixed platform

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -3,33 +3,6 @@
 
 #include "WindowInitiator.hpp"
 
-#ifdef _WIN64
-int main(const int argc, char* argv[]) {
-    std::string backgroundColor = "black";
-    auto shouldExitOnKeyPress = false;
-
-    std::map<std::string, std::string*> argMap = {
-        {"-c", &backgroundColor},
-        {"--color", &backgroundColor},
-        {"-dke", reinterpret_cast<std::string*>(&shouldExitOnKeyPress)},
-        {"--disable-key-exit", reinterpret_cast<std::string*>(&shouldExitOnKeyPress)}
-    };
-
-    for (auto i = 1; i < argc; ++i) {
-        if (std::string currentArg = argv[i]; argMap.contains(currentArg)) {
-            if (currentArg == "-dke" || currentArg == "--disable-key-exit") {
-                shouldExitOnKeyPress = true;
-            } else {
-                *argMap[currentArg] = argv[++i];
-            }
-        }
-    }
-
-    WindowInitiator windowInitiator(backgroundColor, shouldExitOnKeyPress);
-    windowInitiator.createWindow();
-    return 0;
-}
-#elif defined(_WIN32)
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     std::string backgroundColor = "black";
     auto shouldExitOnKeyPress = false;
@@ -63,4 +36,3 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     windowInitiator.createWindow();
     return 0;
 }
-#endif
